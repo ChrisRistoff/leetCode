c3d2b1b2cfb1b089f199ad5ddd903d109fc509c9
Krasyo <doanusho@gmail.com>
2023-08-07 18:04:53 +0100

    palindrome number solution O(n)

---
+++ typescript/easy/palindromeNumber.ts
@@ -0,0 +1,18 @@
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+function isPalindrome(x: number): boolean {
+  const str = x.toString();
+  let reversed = "";
+
+  for (let i = str.length-1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return reversed === str;
+}
+
+console.log(isPalindrome(121)); // true
+console.log(isPalindrome(-121)); // false
+console.log(isPalindrome(10)); // false
+console.log(isPalindrome(-101)); // false
+console.log(isPalindrome(123321)) // true
