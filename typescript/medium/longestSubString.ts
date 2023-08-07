2223514474e0b359e1f1d9b56c2ae4205c30b208
Krasyo <doanusho@gmail.com>
2023-08-07 17:51:42 +0100

    longest substring without repeated characters O(n)

---
+++ typescript/medium/longestSubString.ts
@@ -0,0 +1,30 @@
+// given a string s, find the length of the longest substring without repeating character
+
+function lengthOfLongestSubstring(s: string): number {
+
+  const charSet = new Set();
+  let left = 0;
+  let right = 0;
+  let maxLength = 0;
+
+  while (right < s.length) {
+    if (!charSet.has(s[right])) {
+      charSet.add(s[right]);
+
+      if (maxLength < right - left + 1) {
+        maxLength = right - left + 1;
+      }
+
+      right++;
+
+    } else {
+      charSet.delete(s[left]);
+      left++;
+    }
+  }
+    return maxLength;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb")); // 3
+console.log(lengthOfLongestSubstring("bbbbb")); // 1
+console.log(lengthOfLongestSubstring("pwwkew")); // 3
