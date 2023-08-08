33c5c2a084b452f265c42180b30d3d7108058ac1
Krasyo <doanusho@gmail.com>
2023-08-08 17:57:37 +0100

    is palindrome for letters, I've done this before but there was a slight twist, had to remove all non letter characters.

--- typescript/dist/easy/bestTimeToBuyStock.js
+++ typescript/dist/easy/bestTimeToBuyStock.js
@@ -1,10 +1,17 @@
-/* You are given an array prices where prices[i] is the price of a given stock on the ith day.
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
 You want to maximize your profit by choosing a single day to buy one stock and choosing a
 different day in the future to sell that stock.
 
 Return the maximum profit you can achieve from this transaction.
-If you cannot achieve any profit, return 0. */
+If you cannot achieve any profit, return 0.
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+*/
 function maxProfit(prices) {
     const map = new Map();
     let min = prices[0];
@@ -30,3 +37,23 @@ function maxProfit(prices) {
     return profit;
 }
 console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 5
+console.log(maxProfit([7, 6, 4, 3, 1])); // 0
+// faster solution
+function maxProfit2(prices) {
+    let minPrice = prices[0];
+    let maxProfit = 0;
+    for (let i = 1; i < prices.length; i++) {
+        const currentPrice = prices[i];
+        // Calculate the potential profit by selling at the current price
+        const potentialProfit = currentPrice - minPrice;
+        // Update the maximum profit if the potential profit is greater
+        if (potentialProfit > maxProfit) {
+            maxProfit = potentialProfit;
+        }
+        // Update the minimum price if a lower price is encountered
+        if (currentPrice < minPrice) {
+            minPrice = currentPrice;
+        }
+    }
+    return maxProfit;
+}
---
+++ typescript/easy/validPali.ts
@@ -0,0 +1,26 @@
+/* A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome. */
+
+function isItPalindrome(s: string): boolean {
+
+  // remove all non-alphanumeric characters
+  let newStr = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let strL = newStr.split(" ");
+  newStr = strL.join("");
+
+  let revStr = "";
+
+  // reverse the string
+  for (let i = newStr.length - 1; i >= 0; i--) {
+    revStr += newStr[i];
+  }
+
+  return newStr === revStr;
+}
+
+console.log(isItPalindrome("A man, a plan, a canal: Panama")); // true
