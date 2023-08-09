793050bdfe8c63bde9a77ae30d0457616de49fff
Krasyo <doanusho@gmail.com>
2023-08-09 21:31:59 +0100

    reverse integer, 32-bit integer range constraint. O(N) time complexity. Not sure how this is medium while some others are easy...

---
+++ typescript/medium/reverseInteger.ts
@@ -0,0 +1,48 @@
+/*
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the
+value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+Input: x = 123
+Output: 321
+
+Input: x = -123
+Output: -321
+
+Input: x = 120
+Output: 21
+ */
+
+function reverse(x: number) {
+
+  const string = x.toString();
+  const length = string.length;
+  let reversed = "";
+
+  if (length === 1) return x;
+
+
+  for (let i = length - 1; i >= 0; i--) {
+    if (reversed === "" && string[i] === "0") continue;
+    if (string[i] === "-") {
+      reversed = string[i] + reversed;
+      continue;
+    }
+
+    reversed += string[i];
+  }
+
+  const revNum = parseInt(reversed);
+
+  if (revNum > 2147483647 || revNum < -2147483648) return 0;
+
+  return revNum;
+}
+
+
+console.log(reverse(123));
+console.log(reverse(-123));
+console.log(reverse(120));
+console.log(reverse(0));
+console.log(reverse(1534236469));
