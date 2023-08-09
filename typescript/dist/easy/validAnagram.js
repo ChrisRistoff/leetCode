2a3a4a1f218b558ce9043137db14f154b8813ce2
Krasyo <doanusho@gmail.com>
2023-08-09 21:11:21 +0100

    valid anagram challenge, using map again as that is my new golden hammer, better than nested for loops as it was before. Time complexity is O(N)

---
+++ typescript/dist/easy/validAnagram.js
@@ -0,0 +1,37 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+typically using all the original letters exactly once.
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+ */
+function isAnagram(s, t) {
+    const map = new Map();
+    const len = s.length;
+    if (len !== t.length) {
+        return false;
+    }
+    for (let i = 0; i < len; i++) {
+        const char = s[i];
+        const count = map.get(char) || 0;
+        map.set(char, count + 1);
+    }
+    for (let i = 0; i < len; i++) {
+        const char = t[i];
+        const count = map.get(char) || 0;
+        if (count === 0) {
+            return false;
+        }
+        map.set(char, count - 1);
+    }
+    return true;
+}
+;
+console.log(isAnagram('anagram', 'nagaram'));
+console.log(isAnagram('rat', 'car'));
+console.log(isAnagram('a', 'ab'));
+console.log(isAnagram('ab', 'a'));
+console.log(isAnagram('aacc', 'ccac'));
+console.log(isAnagram('aaccaaaaaa', 'caaaaaaaac'));
