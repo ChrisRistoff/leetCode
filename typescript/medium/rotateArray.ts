3c12fbe26e25d622a16dafe7f1463d4282d8ee8d
Krasyo <doanusho@gmail.com>
2023-08-19 13:16:00 +0100

    rotate array "in place", cheeky solution but it somehow scored 98.30%, pretty bad on memory but it works. O(N).
    My first solution was popping and unshifting but that scored bottom 10%,
    although it did score top for memory.

---
+++ typescript/medium/rotateArray.ts
@@ -0,0 +1,47 @@
+/*
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+*/
+
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+  let temp: number[] = [];
+  let len = nums.length;
+  k = k % len;
+
+  for (let i = len - k  ; i < len; i++) {
+    temp.push(nums[i]);
+  }
+
+  for (let i = 0; i < len - k; i++) {
+    temp.push(nums[i]);
+  }
+
+  for (let i = 0; i < len; i++) {
+    nums[i] = temp[i];
+  }
+
+  console.log(nums);
+
+};
+
+// time complexity: O(n)
+
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3)); // [5,6,7,1,2,3,4]
