2c153baee86e676b11ac169aa6a21f4c6128dfb8
Krasyo <doanusho@gmail.com>
2023-08-26 12:49:54 +0100

    valid partitions problem, definitely not solved, O(N) but doesn't work, adding it to ask for help

---
+++ typescript/medium/validPartition.ts
@@ -0,0 +1,61 @@
+/*
+You are given a 0-indexed integer array nums. You have to partition the array into one or more
+contiguous subarrays.
+
+We call a partition of the array valid if each of the obtained subarrays satisfies one of
+the following conditions:
+
+The subarray consists of exactly 2, equal elements. For example, the subarray [2,2] is good.
+The subarray consists of exactly 3, equal elements. For example, the subarray [4,4,4] is good.
+The subarray consists of exactly 3 consecutive increasing elements, that is,
+the difference between adjacent elements is 1. For example, the subarray [3,4,5] is good,
+but the subarray [1,3,5] is not.
+Return true if the array has at least one valid partition. Otherwise, return false.
+
+Example 1:
+Input: nums = [4,4,4,5,6]
+Output: true
+Explanation: The array can be partitioned into the subarrays [4,4] and [4,5,6].
+This partition is valid, so we return true.
+
+Example 2:
+Input: nums = [1,1,1,2]
+Output: false
+Explanation: There is no valid partition for this array.
+*/
+
+function validPartition(nums: number[]): boolean {
+
+  let len = nums.length
+  let i = 0
+
+  while (i<len) {
+    let current = nums[i]
+    let next = nums[i+1]
+    let nextNext = nums[i+2]
+
+    if (current === next && next === nextNext) {
+      i += 3
+
+    } else if (current === next) {
+      i += 2
+
+    } else if (current === next-1 && next === nextNext-2) {
+      i += 3
+
+    } else if (current === next+1) {
+      i +=2
+
+    } else {
+      return false
+    }
+  }
+
+  return true
+};
+
+console.log(validPartition([4,4,4,5,6])) // true
+console.log(validPartition([1,1,1,2])) // false
+console.log(validPartition([1,2,3,3,3,2,1])) // true
+console.log(validPartition([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])) // true
+console.log(validPartition([1,2,3,4,5,6,7,8,9,10,11,12,13,14,16])) // false
